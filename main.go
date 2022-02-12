0b7d34f1410d5113498e311fde0baf21d7a09946
Johnson <poiuytrengo@qq.com>
2022-02-12 21:03:06 +0800

    commit

--- main.go
+++ main.go
@@ -1,25 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/hello", helloHandler)
-	err := http.ListenAndServe(":9000", nil)
+	err := http.ListenAndServe(":9000", &Engine{})
 	if err != nil {
 		panic(err)
-	}
-}
-
-func indexHandler(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "URL.Path = %q\n", request.URL.Path)
-}
-
-func helloHandler(writer http.ResponseWriter, request *http.Request) {
-	for k, v := range request.Header {
-		fmt.Fprintf(writer, "Header[%q] = %q\n", k, v)
 	}
 }
